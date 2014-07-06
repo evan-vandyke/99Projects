1f9850831534ab5cc8c00dc3d21e5ab7b7bb0045
Evan VanDyke <vandyke09@gmail.com>
2014-07-06 18:54:14 -0400

    finish pig latin

---
+++ Text/pig_latin.rb
@@ -0,0 +1,19 @@
+# Pig Latin - Pig Latin is a game of alterations played on 
+# the English language game. To create the Pig Latin form 
+# of an English word the initial consonant sound is transposed 
+# to the end of the word and an ay is affixed 
+# (Ex.: "banana" would yield anana-bay). 
+
+puts "Enter word to git Pig Latin version"
+word = gets.strip
+
+vowels = "a e i o u y".split(" ")
+
+if vowels.include? word[0].downcase
+	word << "-way"
+else
+	c = word[0]
+	word[0] = ''
+	word << '-' << c << 'ay'
+end
+puts word
---
+++ Text/reverse_string.rb
@@ -0,0 +1,7 @@
+# Reverse a String - Enter a string and
+#  the program will reverse it and print it out.
+
+puts "Enter a word to reverse"
+answer = gets.strip
+
+puts answer.reverse
